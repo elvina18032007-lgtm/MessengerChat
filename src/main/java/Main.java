d9e0f39906aef2b6f3dfeb3b3cbd184cfbbcd501
Malika <malinonim@gmail.com>
2025-11-25 21:06:59 +0300

    в Main все разделено на методы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static User[] masUsers = new User[5];
     public static Group[] groups = new Group[2];
     public static User admin = new User();
-    public static User[] users = new User[masUsers.length-1];
+    public static User[] users = new User[masUsers.length - 1];
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -22,10 +22,11 @@ public class Main {
             User user = new User(scanner.nextLine());
             masUsers[i] = user;
         }
-        // выбор от чьего лица пишем
+        // выбор от чьего лица пишем и создание списка остальных
         System.out.println("Выберете ваш аккаунт:");
         User.toPrintUsers(masUsers);
         admin = new User(scanner.nextLine());
+        users = User.getUsers(masUsers, admin);
 
         // создание групп
         Message[] messages = new Message[]{new Message("приветики", "вчера", masUsers[0]),
@@ -37,32 +38,44 @@ public class Main {
         groups[0] = group1;
         groups[1] = group2;
 
+        // запуск программы
         menu();
-        users = User.getUsers(masUsers, admin);
         String action = scanner.nextLine(); //тип ввод операции, которую хочет совершить админ
 
         while (!action.equals("Exit")) {
             if (action.equals("Зайти на канал")) {
-                System.out.println("Выбери чей канал хочешь посетить: ");
-                User.toPrintUsers(users);
-                System.out.println(admin.codeAdmin(masUsers, admin) + "Мой канал" + "\u001B[0m");
-                User choice = new User(scanner.nextLine());
-                Channel.posts(choice, admin, masUsers, "");
-            }
-            if (action.equals("Сделать новый пост")) {
-                System.out.println("Введите новый пост:");
-                String post = scanner.nextLine();
-                Channel.posts(new User("Мой канал"), admin, masUsers, post);
-            }
-            if (action.equals("Написать сообщение")) {
+                enterTheChannel();
+            } else if (action.equals("Сделать новый пост")) {
+                makeNewPost();
+            } else if (action.equals("Написать сообщение")) {
                 writeAMessage();
+            } else {
+                System.out.println("Некорректный ввод команды");
             }
             menu();
             action = scanner.nextLine();
         }
     }
-    public static void writeAMessage(){
-        Scanner scanner = new Scanner(System.in);;
+
+    public static void enterTheChannel() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выбери чей канал хочешь посетить: ");
+        User.toPrintUsers(users);
+        System.out.println(admin.codeAdmin(masUsers, admin) + "Мой канал" + "\u001B[0m");
+        User choice = new User(scanner.nextLine());
+        Channel.posts(choice, admin, masUsers, "");
+    }
+
+    public static void makeNewPost() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите новый пост:");
+        String post = scanner.nextLine();
+        Channel.posts(new User("Мой канал"), admin, masUsers, post);
+    }
+
+    public static void writeAMessage() {
+        Scanner scanner = new Scanner(System.in);
+        ;
         System.out.println("Выбери, куда написать: \"личные сообщения\" или \"группы\"");
         String toWhere = scanner.nextLine();
         if (toWhere.equals("личные сообщения")) {
@@ -70,13 +83,12 @@ public class Main {
             User.toPrintUsers(masUsers);
             String chat = scanner.nextLine();
             System.out.println();
-        }
-        else if (toWhere.equals("группы")) {
+        } else if (toWhere.equals("группы")) {
             System.out.println("Выбери группу:");
             Group.printGroups(groups);
             String chat = scanner.nextLine();
-            for (Group group : groups){
-                if (group.getName().equals(chat)){
+            for (Group group : groups) {
+                if (group.getName().equals(chat)) {
                     group.printMessages();
                     System.out.println("Введите сообщение...");
                     group.sendMessage(scanner.nextLine(), "сегодня", admin);
@@ -84,10 +96,7 @@ public class Main {
                     break;
                 }
             }
-        }
-        else {
-            System.out.println("Некорректный ввод");
-        }
+        } else System.out.println("Некорректный ввод");
     }
 
     public static void menu() {
