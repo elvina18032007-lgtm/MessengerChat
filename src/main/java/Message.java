4c1119afbd1d471ff2dcc12c395e519e6ff56e69
Malika <malinonim@gmail.com>
2025-11-25 12:32:44 +0300

    создан Message без комментариев

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,32 +1,32 @@
 public class Message {
     protected String text;
     protected String date;
-//    protected User sender;
-//    protected User receiver;
-//
-//    public Message(){
-//        this.text = "";
-//        this.date = "";
-//        this.sender = new User();
-//        this.receiver = new User();
-//    }
-//
-//    public Message(String text, String date, User sender, User receiver) {
-//        this.text = text;
-//        this.date = date;
-//        this.sender = sender;
-//        this.receiver = receiver;
-//    }
-//
-//    public Message(String text, String date, User sender) {
-//        this.text = text;
-//        this.date = date;
-//        this.sender = sender;
-//    }
+    protected User sender;
+    protected User receiver;
 
-//    public String toString(){
-//        return sender + " |" + date + "|" + ":\n" + text;
-//    }
+    public Message(){
+        this.text = "";
+        this.date = "";
+        this.sender = new User();
+        this.receiver = new User();
+    }
+
+    public Message(String text, String date, User sender, User receiver) {
+        this.text = text;
+        this.date = date;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public Message(String text, String date, User sender) {
+        this.text = text;
+        this.date = date;
+        this.sender = sender;
+    }
+
+    public String toString(){
+        return sender + " |" + date + "|" + ":\n" + text;
+    }
 
     public String getText() {
         return text;
